b7321933ec1d3fa7e015bbca52a233a0d2a62232
3424 <3424@example.com>
2022-11-04 09:33:04 +0900

    リファクタリング

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
-    #以下追記
+
+    before_action :require_login, only: [:show, :destroy]
+
     def new
       @user = User.new
     end
@@ -7,19 +9,19 @@ class UsersController < ApplicationController
     def create
       @user = User.new(user_params)
       if @user.save
-        redirect_to profile_path(@user)
+        log_in(@user)
+        redirect_to profile_path
       else
         render 'new'
       end
     end
 
     def show
-     @user = User.find(params[:id])
+     @user = User.find(current_user.id)
     end
 
     def destroy
-      user = User.find(params[:id])
-      user.destroy
+      current_user.destroy
       redirect_to signup_path
     end
 
