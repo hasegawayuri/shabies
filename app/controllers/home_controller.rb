a46ee324ed1cd4b7ab3606a5dd9a3b8f49185b21
3424 <3424@example.com>
2022-12-07 10:29:30 +0900

    検索画面　修正

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,6 +8,9 @@ class HomeController < ApplicationController
     @members=Member.all
   end
   def search
-    
+    session[:search_commit] = nil
+    session[:search_keyword] = nil
+    session[:search_classification] = nil
+    session[:search_hash_tag] = nil
   end  
 end
