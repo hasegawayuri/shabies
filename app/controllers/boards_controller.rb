6736058b446f26da1032b273a6fb53e06917374b
3414 <3414@example.com>
2023-01-11 10:59:24 +0900

    掲示板

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -57,6 +57,11 @@ class BoardsController < ApplicationController
     end
   end
 
+  def get_image
+    board = Board.find(params[:id])
+    send_data(board.image,disposition: :inline)
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_board
